93d36268dabf1e75e006a63639ae5275bfd9646b
Andre Maree <andre.maree@mintgroup.net>
2022-07-07 09:23:05 +0200

    Added namespaces and cleaned up js version

--- Scrambler/Program.cs
+++ Scrambler/Program.cs
@@ -2,8 +2,21 @@
 // There is a Javascript version which can be used in a browser in the included file scrambler.htm
 // Text can be obfuscated in Javascript and passed to C# and reverse the obfuscation, and vice versa
 
-string key = "Gl6Nh1O.P2QeR3Safn5o{pi4q'rsyz@AYCDb:cdE0FHtvJw9xIKgk}mLMBTZ8VWj7X";
+namespace Scrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "Gl6Nh1O.P2QeR3Safn5o{pi4q'rsyz@AYCDb:cdE0FHtvJw9xIKgk}mLMBTZ8VWj7X";
 
-string scrambled = Scrambler.Scrambler.Scramble("andre.maree@engagementfactory.com", key);
+            string scrambled = Scrambler.Scramble("scramblertest@gmail.com", key);
+            Console.WriteLine(scrambled);
 
-string unscrambled = Scrambler.Scrambler.UnScramble(scrambled, key);
+            string unscrambled = Scrambler.UnScramble(scrambled, key);
+            Console.WriteLine(unscrambled);
+
+            Console.ReadKey();
+        }
+    }
+}
